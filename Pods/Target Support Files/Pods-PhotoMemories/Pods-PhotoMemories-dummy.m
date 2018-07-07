e822731cf17240e0896983645f5f02fc77ff51de
Nguyen Manh Tuan <tuanhust@Admins-Mac-mini.local>
2018-07-08 00:26:32 +0700

    test

--- Pods/Target Support Files/Pods-PhotoMemories/Pods-PhotoMemories-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoMemories : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoMemories
-@end
--- Pods/Target Support Files/Pods-PhotoMemoriesUITests/Pods-PhotoMemoriesUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PhotoMemoriesUITests : NSObject
-@end
-@implementation PodsDummy_Pods_PhotoMemoriesUITests
-@end
